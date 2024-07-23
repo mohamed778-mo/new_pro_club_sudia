839c09e121fa64539c381ace98b2d2cd2963540e
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2024-07-23 16:52:00 +0300

    Update audience.js

--- routers/audience.js
+++ routers/audience.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const { admin_auth} = require('../middleware/auth')
   
 const {
-    create_month ,create_day ,audience_for_players ,audience_for_coachs,getAttendees,delete_month,get_all_month,get_month
+    create_month ,create_day ,audience_for_players ,audience_for_coachs,getAttendees,delete_month,get_all_month,get_month,deleteDayById
   } = require('../controllers/audience')
 
 
@@ -14,7 +14,9 @@ const {
   router.post("/admin/audience_for_coach/:month_id/:day_id",admin_auth,audience_for_coachs)
   router.get("/get_audience/:month_id/:day_id",admin_auth,getAttendees)
   router.get("/get_all_month",admin_auth,get_all_month)
-router.get("/get_month/:month_id",admin_auth,get_month)
- router.delete("/delete_month/:month_id",admin_auth,delete_month)
+  router.get("/get_month/:month_id",admin_auth,get_month)
+  router.delete("/delete_month/:month_id",admin_auth,delete_month)
+
+  router.delete("/delete_day/:month_id/:day_id",admin_auth,deleteDayById)
 
   module.exports = router;
